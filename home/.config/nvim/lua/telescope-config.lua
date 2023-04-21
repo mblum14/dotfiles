e349fcd5a41bbf42d7366abd60fc5faa11d8b59e
Michael Blumberg <1111642+mblum14@users.noreply.github.com>
2023-04-21 12:50:08 -0400

    Create telescope-config.lua

---
+++ home/.config/nvim/lua/telescope-config.lua
@@ -0,0 +1,21 @@
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+local M = {}
+
+M.git_or_find_files = function()
+  local opts = {
+    cwd = vim.fn.systemlist("git rev-parse --show-toploevel")[1]
+  }
+  local ok = pcall(require"telescope.builtin".git_files, opts)
+  if not ok then require"telescope.builtin".find_files(opts) end
+end
+
+M.live_grep_from_root = function()
+  local opts = {
+    cwd = vim.fn.systemlist("git rev-parse --show-toploevel")[1]
+  }
+  require"telescope.builtin".live_grep(opts)
+end
+
+return M
