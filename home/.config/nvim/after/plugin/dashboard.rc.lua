55e7498b1e773c8374daff2d89d90cb0e223836c
Michael Blumberg <mblum14@gmail.com>
2023-04-23 21:34:32 -0400

    update vim dashboard

--- home/.config/nvim/after/plugin/dashboard.rc.lua
+++ home/.config/nvim/after/plugin/dashboard.rc.lua
@@ -4,23 +4,12 @@ if (not present) then return end
 local dashboard = require "alpha.themes.dashboard"
 local function header()
   return {
-    [[                                           bbbbbbbb            ]],
-    [[                                           b::::::b            ]],
-    [[                                           b::::::b            ]],
-    [[                                           b::::::b            ]],
-    [[                                            b:::::b            ]],
-    [[nnnn  nnnnnnnn    vvvvvvv           vvvvvvv b:::::bbbbbbbbb    ]],
-    [[n:::nn::::::::nn   v:::::v         v:::::v  b::::::::::::::bb  ]],
-    [[n::::::::::::::nn   v:::::v       v:::::v   b::::::::::::::::b ]],
-    [[nn:::::::::::::::n   v:::::v     v:::::v    b:::::bbbbb:::::::b]],
-    [[  n:::::nnnn:::::n    v:::::v   v:::::v     b:::::b    b::::::b]],
-    [[  n::::n    n::::n     v:::::v v:::::v      b:::::b     b:::::b]],
-    [[  n::::n    n::::n      v:::::v:::::v       b:::::b     b:::::b]],
-    [[  n::::n    n::::n       v:::::::::v        b:::::b     b:::::b]],
-    [[  n::::n    n::::n        v:::::::v         b:::::bbbbbb::::::b]],
-    [[  n::::n    n::::n         v:::::v          b::::::::::::::::b ]],
-    [[  n::::n    n::::n          v:::v           b:::::::::::::::b  ]],
-    [[  nnnnnn    nnnnnn           vvv            bbbbbbbbbbbbbbbb   ]],
+    [[                               __                ]],
+    [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+    [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+    [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+    [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+    [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
   }
 end
 
