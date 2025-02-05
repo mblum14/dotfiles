2d762bc901f67b97aa1f08dfd40af20d7c810ad8
mblum14 <mblum14@gmail.com>
2025-02-05 13:47:30 -0500

    feat(awsume): Add bash completion

---
+++ dot_config/bashrc.d/completion.d/awsume.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+_awsume() {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(awsume-autocomplete)
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+complete -F _awsume awsume
