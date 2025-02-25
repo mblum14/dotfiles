4bed04a3917d49798106f42322204cf1c0bc2d88
mblum14 <mblum14@gmail.com>
2025-02-25 16:50:49 -0500

    fix(prompt): Remove aws-sso-util completion for now

--- private_dot_config/bashrc.d/completion.d/aws-sso-util.sh
+++ private_dot_config/bashrc.d/completion.d/aws-sso-util.sh
@@ -1,28 +1,31 @@
-_aws_sso_util_completion() {
-  local IFS=$'\n'
-  local response
+#!/usr/bin/env bash
 
-  response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _AWS_SSO_UTIL_COMPLETE=bash_complete $1)
-
-  for completion in $response; do
-    IFS=',' read type value <<<"$completion"
-
-    if [[ $type == 'dir' ]]; then
-      COMPREPLY=()
-      compopt -o dirnames
-    elif [[ $type == 'file' ]]; then
-      COMPREPLY=()
-      compopt -o default
-    elif [[ $type == 'plain' ]]; then
-      COMPREPLY+=($value)
-    fi
-  done
-
-  return 0
-}
-
-_aws_sso_util_completion_setup() {
-  complete -o nosort -F _aws_sso_util_completion aws-sso-util
-}
-
-_aws_sso_util_completion_setup
+#_aws_sso_util_completion() {
+#  local IFS=$'\n'
+#  local response
+#
+#  response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _AWS_SSO_UTIL_COMPLETE=bash_complete $1)
+#
+#  for completion in $response; do
+#    IFS=',' read type value <<<"$completion"
+#
+#    if [[ $type == 'dir' ]]; then
+#      COMPREPLY=()
+#      compopt -o dirnames
+#    elif [[ $type == 'file' ]]; then
+#      COMPREPLY=()
+#      compopt -o default
+#    elif [[ $type == 'plain' ]]; then
+#      COMPREPLY+=($value)
+#    fi
+#  done
+#
+#  return 0
+#}
+#
+#_aws_sso_util_completion_setup() {
+# FIXME: sort out the "nosort" issue
+#  complete -o nosort -F _aws_sso_util_completion aws-sso-util
+#}
+#
+#_aws_sso_util_completion_setup
