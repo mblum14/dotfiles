63c9248effae75b198aab4cedd1b4f03abc2baa4
Michael Blumberg <mblum14@gmail.com>
2025-04-11 10:09:57 -0400

    feat(nvim): Auto select python venv

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -10,3 +10,14 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   desc = "Validate Gitlab CI  files on save",
   pattern = ".gitlab-ci.yml",
 })
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Auto select virtualenv Nvim open",
+  pattern = "*",
+  callback = function()
+    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+    if venv ~= "" then
+      require("venv-selector").retrieve_from_cache()
+    end
+  end,
+  once = true,
+})
