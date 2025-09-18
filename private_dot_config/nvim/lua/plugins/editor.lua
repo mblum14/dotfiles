5fd0a89e8f017508185e7766c352b703ceaaab10
Michael Blumberg <mblum14@gmail.com>
2025-09-18 08:56:45 -0400

    chore(nvim): Update plugins

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -2,7 +2,7 @@ return {
   {
     "stevearc/oil.nvim",
     dependencies = {
-      { "echasnovski/mini.icons", opts = {} },
+      { "nvim-mini/mini.icons", opts = {} },
     },
     config = function()
       require("oil").setup({
