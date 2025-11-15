12574e175f575f4d6eae7b5017ffdfde451380e6
Michael Blumberg <mblum14@gmail.com>
2025-11-14 22:24:58 -0500

    fix(nvim): Configure sidekick.nvim

--- private_dot_config/nvim/lua/plugins/ai.lua
+++ private_dot_config/nvim/lua/plugins/ai.lua
@@ -1,8 +1,132 @@
+--{
+--  "yetone/avante.nvim",
+--  opts = {
+--    provider = "codex",
+--  },
+--},
 return {
+  desc = "Next edit suggestions with the Copilot LSP server",
+
+  -- lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local icons = {
+        Error = { " ", "DiagnosticError" },
+        Inactive = { " ", "MsgArea" },
+        Warning = { " ", "DiagnosticWarn" },
+        Normal = { LazyVim.config.icons.kinds.Copilot, "Special" },
+      }
+      table.insert(opts.sections.lualine_x, 2, {
+        function()
+          local status = require("sidekick.status").get()
+          return status and vim.tbl_get(icons, status.kind, 1)
+        end,
+        cond = function()
+          return require("sidekick.status").get() ~= nil
+        end,
+        color = function()
+          local status = require("sidekick.status").get()
+          local hl = status and (status.busy and "DiagnosticWarn" or vim.tbl_get(icons, status.kind, 2))
+          return { fg = Snacks.util.color(hl) }
+        end,
+      })
+
+      table.insert(opts.sections.lualine_x, 2, {
+        function()
+          local status = require("sidekick.status").cli()
+          return " " .. (#status > 1 and #status or "")
+        end,
+        cond = function()
+          return #require("sidekick.status").cli() > 0
+        end,
+        color = function()
+          return { fg = Snacks.util.color("Special") }
+        end,
+      })
+    end,
+  },
+
   {
     "folke/sidekick.nvim",
     opts = function()
       require("sidekick.nes").disable()
     end,
+    -- stylua: ignore
+    keys = {
+      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+      {
+        "<c-.>",
+        function() require("sidekick.cli").toggle() end,
+        desc = "Sidekick Toggle",
+        mode = { "n", "t", "i", "x" },
+      },
+      {
+        "<leader>aa",
+        function() require("sidekick.cli").toggle() end,
+        desc = "Sidekick Toggle CLI",
+      },
+      {
+        "<leader>as",
+        function() require("sidekick.cli").select() end,
+        -- Or to select only installed tools:
+        -- require("sidekick.cli").select({ filter = { installed = true } })
+        desc = "Select CLI",
+      },
+      {
+        "<leader>ad",
+        function() require("sidekick.cli").close() end,
+        desc = "Detach a CLI Session",
+      },
+      {
+        "<leader>at",
+        function() require("sidekick.cli").send({ msg = "{this}" }) end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>af",
+        function() require("sidekick.cli").send({ msg = "{file}" }) end,
+        desc = "Send File",
+      },
+      {
+        "<leader>av",
+        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
+        "<leader>ap",
+        function() require("sidekick.cli").prompt() end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
+      },
+    },
+  },
+
+  {
+    "folke/snacks.nvim",
+    optional = true,
+    opts = {
+      picker = {
+        actions = {
+          sidekick_send = function(...)
+            return require("sidekick.cli.picker.snacks").send(...)
+          end,
+        },
+        win = {
+          input = {
+            keys = {
+              ["<a-a>"] = {
+                "sidekick_send",
+                mode = { "n", "i" },
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }
