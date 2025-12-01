8c90efbbaa11652a3c7f354c3bf2156351701c4c
Michael Blumberg <mblum14@gmail.com>
2025-12-01 13:09:57 -0500

    chore(nvim): Install headhunter

--- private_dot_config/nvim/lua/plugins/code.lua
+++ private_dot_config/nvim/lua/plugins/code.lua
@@ -5,4 +5,20 @@ return {
       keymap = { preset = "default" },
     },
   },
+  {
+    "StackInTheWild/headhunter.nvim",
+    config = function()
+      require("headhunter").setup({
+        enabled = true,
+        keys = {
+          prev = "[g",
+          next = "]g",
+          take_head = "<leader>gh",
+          take_origin = "<leader>go",
+          take_both = "<leader>gb",
+          quickfix = "<leader>gq",
+        },
+      })
+    end,
+  },
 }
