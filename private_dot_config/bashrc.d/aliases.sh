c97b8bd1fefd874dde8f92766f0fb4263f2e5d85
Michael Blumberg <mblum14@gmail.com>
2026-07-17 08:26:22 -0400

    chore(terragrunt): update aliases

--- private_dot_config/bashrc.d/aliases.sh
+++ private_dot_config/bashrc.d/aliases.sh
@@ -40,14 +40,14 @@ alias tf="terraform"
 
 # terragrunt
 alias tg="terragrunt"
-alias unlock="terragrunt force-unlock -force"
-alias plan="terragrunt plan --source-update --download-dir ~/.terragrunt-cache"
-alias apply="terragrunt apply --source-update --download-dir ~/.terragrunt-cache"
-alias output="terragrunt output --source-update --download-dir ~/.terragrunt-cache"
-alias init="terragrunt init --source-update --download-dir ~/.terragrunt-cache"
-alias import="terragrunt import --source-update --download-dir ~/.terragrunt-cache"
-alias state="terragrunt state --source-update --download-dir ~/.terragrunt-cache"
-alias destroy="terragrunt destroy --source-update --download-dir ~/.terragrunt-cache"
+alias tg.unlock="terragrunt force-unlock -force"
+alias tg.plan="terragrunt plan --source-update --download-dir ~/.terragrunt-cache"
+alias tg.apply="terragrunt apply --source-update --download-dir ~/.terragrunt-cache"
+alias tg.output="terragrunt output --source-update --download-dir ~/.terragrunt-cache"
+alias tg.init="terragrunt init --source-update --download-dir ~/.terragrunt-cache"
+alias tg.import="terragrunt import --source-update --download-dir ~/.terragrunt-cache"
+alias tg.state="terragrunt state --source-update --download-dir ~/.terragrunt-cache"
+alias tg.destroy="terragrunt destroy --source-update --download-dir ~/.terragrunt-cache"
 
 # aws profiles
 alias ea-infra='aws.login ea-infra'
