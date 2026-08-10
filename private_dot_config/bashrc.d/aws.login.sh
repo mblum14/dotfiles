a54045d496e480fe64b1bbb8853d39dfc7b4e115
Michael Blumberg <mblum14@gmail.com>
2026-08-10 07:44:58 -0400

    fix(aws.login): reliably reset expired credentials

--- private_dot_config/bashrc.d/aws.login.sh
+++ private_dot_config/bashrc.d/aws.login.sh
@@ -1,5 +1,3 @@
-source "$HOME/.local/bin/lib/log.sh"
-
 # Execute a command with an AWS profile or set the AWS_PROFILE to the target
 function aws.login() {
   # Check if the AWS_PROFILE argument is provided
@@ -11,14 +9,7 @@ function aws.login() {
   local profile="$1"
   shift
 
-  if [[ $# -eq 0 ]]; then
+  if aws sso login --profile "${profile}"; then
     export AWS_PROFILE="${profile}"
   fi
-
-  if ! aws sts get-caller-identity --query "Account" --profile "${profile}" >/dev/null 2>&1; then
-    log::info "AWS Session expired, requesting credentials for ${profile}"
-    aws sso login --profile "${profile}"
-  fi
-
-  # Execute the command with AWS_PROFILE set
 }
